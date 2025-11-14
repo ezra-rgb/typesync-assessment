94e08cf033fb0e6dd5a7ccad65c1ef057a857709
ezra-rgb <ezra@typesync.io>
2025-11-14 01:07:21 +0000

    Fix Express wildcard route pattern

--- frontend/server.js
+++ frontend/server.js
@@ -11,7 +11,7 @@ const distPath = path.join(__dirname, 'dist');
 app.use(express.static(distPath));
 
 // SPA fallback: serve index.html for all non-asset routes
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
 
